a4c8bfcf17319ec50ddbbc91837fb1a52dd73471
henry <958976577@qq.com>
2019-04-14 18:51:05 +0800

    sum

--- projects/test.py
+++ projects/test.py
@@ -3,16 +3,3 @@
 
 
 
-
-# with open('b.txt', mode='a+') as f:
-#     f.seek(0)
-#     f = len(f.readline()s)
-#     print(f)
-
-
-
-
-with open('b.txt', mode='a+') as f:
-    f.seek(0)
-    v = len(f.readline().strip())
-    print(v)
