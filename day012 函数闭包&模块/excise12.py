62c8c6c554d92b32ef5822f7f3e7db7cea435274
henry <958976577@qq.com>
2019-04-15 16:38:42 +0800

    update

--- day012 函数闭包&模块/excise12.py
+++ day012 函数闭包&模块/excise12.py
@@ -390,73 +390,52 @@
 # 5.如果用户在【商品管理】中选择【2】，则让提示让用户输入关键字，输入关键字后根据商品名称进行模糊匹配，如：
 # 6.如果用户在【商品管理】中选择【3】，则提示让用户输入商品名称、价格、数量然后写入到 goods.txt文件
 
-"""
-用户注册和登陆
-"""
-import hashlib
-
-"""
-md5加密
-"""
-def get_data_md5(data):
-    obj = hashlib.md5()
-    obj.update(str(data).strip('[]{}()').encode('utf-8'))
-    v = obj.hexdigest()
-    return v
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 
 """
-用户登陆
+判断文件是否为空
 """
-def user_log_on():
-    flag = "登陆失败"
-    name = input('请输入管理员账号: ')
-    pwd = get_data_md5(input('请输入管理员密码: '))
-    with open('user_list.txt', mode='r', encoding='utf-8') as f:
-        if len(f.readlines(1)) == 0:
-            flag = "用户列表为空文件"
-            print(flag)
-            return flag
-        for i in f:
-            u, v = i.strip().split(':')
-            if name == u and pwd == v:
-                flag = '登陆成功'
-                print(flag)
-                return flag
-    print(flag)
-    return flag
+def just_file_empty(filename):
+    with open(filename, mode='a+') as f:
+        f.seek(0)
+        v = len(f.readline().strip())
+        if v == 0:
+            import os
+            os.remove(filename)
+        return v
 
 """
 商品管理系统
 """
-def mall_manage():
-    TITLE = ['欢迎使用老子的购物商城', '【商品管理】', '【商品列表】', '【录入商品】', '【根据关键字搜索】', '【会员管理】']
+def mall_manage_sys():
+    TITLE = ['欢迎使用老子的购物商城', '【商品管理】', '【商品列表】', \
+             '【录入商品】', '【根据关键字搜索】', '【会员管理】']
     FAULT = "输入不合法，请重新输入"
     PER_PAGE_AMOUNT = 10
-    FILE = 'goods.txt'
 
     """
     1.展示页面
     """
-    def display_pages(arg):
+    def display_pages(data_list):
         j = 1
-        for i in arg:
+        for i in data_list:
             print(str(j) + '.', i)
             j += 1
         print()
-        print('请选择（输入N返回上一级):')
         return
     """
     2.判断用户输入
     """
-    def just_user_input(num, li=None):
+    def just_user_input(num, data_list=None):
         if num.upper() == 'N':
             return 'N'
         # 判断输入是否是可迭代对象的索引
-        elif num.isdecimal() and 0 < int(num) <= len(li):
+        elif num.isdecimal() and 0 < int(num) <= len(data_list):
             num = int(num) - 1
             return num
-        # 判断输入不是空格，回车等字符
+        # 判断输入不是空格，回车等非法字符
         elif len(num.strip()):
             return FAULT
         else:
@@ -467,31 +446,28 @@ def mall_manage():
     3.商品管理
     """
     def goods_manage():
+        FILE = 'goods.txt'
         goods_list = ['查看商品列表', '根据关键字搜索指定商品', '录入商品']
         s = '******' + ''.join(TITLE[:2]) + '******'
-
         """
         1.查看商品列表
         """
         def check_goods_list(filename):
             print('******' + ''.join(TITLE[0:3]) + '******')
-            with open(filename, mode='r', encoding='utf-8') as f:
-                if f.readlines(1):
-                    pass
-                else:
-                    print('没有商品')
-                    input('按任意键继续')
-                    return
-                msg = 1
-                for i in f:
-                    u, v, w = i.strip().split(':')
-                    print(u, ' ', v, ' ', w, ' ')
-                    msg += 1
-                    if msg == PER_PAGE_AMOUNT + 1:
-                        msg = 1
-                        input('按enter显示下一页')
-                input('按任意键返回')
-                return
+            if just_file_empty(filename):
+                with open(filename, mode='r', encoding='utf-8' ) as f:
+                    msg = 1
+                    for i in f:
+                        u, v, w = i.strip().split(':')
+                        print(u, ' ', v, ' ', w, ' ')
+                        msg += 1
+                        if msg == PER_PAGE_AMOUNT + 1:
+                            msg = 1
+                            input('按enter显示下一页')
+                    input('按enter键返回')
+            else:
+                print('没有商品')
+            return
 
         """
         2.根据关键字搜索指定商品
@@ -544,13 +520,14 @@ def mall_manage():
                     if good_name.upper() == 'N':
                         f.close()
                         return
-                    break
+                    else:
+                        break
                 """
                 输入商品价格
                 """
                 while True:
                     good_price = input('请输入商品价格：')
-                    if len(good_price.strip()) == 0:
+                    if len(good_price.strip()) == 0 or not good_price.isdecimal():
                         print(FAULT)
                         continue
                     break
@@ -564,18 +541,18 @@ def mall_manage():
                         print(FAULT)
                         continue
                     break
+
                 li = [good_name, good_price, good_amount]
                 f.write(':'.join(li) + '\n')
                 f.flush()
                 print('添加成功')
-                f.close()
-                return
+                continue
 
         while True:
             print(s)
             display_pages(goods_list)
             goods_funcs = [check_goods_list, check_goods_keys, check_goods_in]
-            num = input('please input your choice: ')
+            num = input('请选择（输入N返会上一级）: ')
             v = just_user_input(num, goods_list)
             if v == 'N':
                 return
@@ -588,190 +565,7 @@ def mall_manage():
     4.会员管理
     """
     def users_manage():
-        PER_PAGE_AMOUNT = 10
-        print('******' + TITLE[0]+TITLE[5] + '******')
-        users_manage_list = ['添加用户', '用户列表', '修改密码', '删除用户']
-        display_pages(users_manage_list)
-
-        """
-        1.1用户注册
-        """
-        def user_add():
-            while True:
-                print('******' + TITLE[0] +'【会员管理】' + '【' + users_manage_list[0] + '】' + '******')
-                user_name = input('输入用户名(N退出): ')
-                if user_name.upper() == 'N':
-                    break
-                if user_name.strip() == '':
-                    print('不合法用户名')
-                    continue
-                """
-                判断用户是否存在，不存在继续，存在则重新输入
-                """
-                with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                    if len(f.readlines(1)) == 1:
-                        a = True
-                        with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                            for i in f:
-                                u, v = i.split(':')
-                                if user_name == u:
-                                    a = False
-                                    print('用户名已存在')
-                                    break
-                            if a:
-                                pass
-                            else:
-                                continue
-                    else:
-                        pass
-
-                """
-                用户设置密码，并加密
-                """
-                while True:
-                    user_pwd = input('请输入密码: ')
-                    user_pwd2 = input('请确认密码: ')
-                    if user_pwd != user_pwd2:
-                        print('两次密码不一致')
-                        continue
-                    else:
-                        break
-                user_pwd = get_data_md5(user_pwd)
-
-                """
-                把用户账号和密码写入文件
-                """
-                f = open('user_list.txt', mode='a', encoding='utf-8')
-                msg = user_name + ':' + user_pwd + '\n'
-                f.write(msg)
-                f.flush()
-            f = open('user_list.txt', mode='a', encoding='utf-8')
-            f.close()
-        """
-        1.2用户列表
-        """
-        def users_list():
-            print('******' + TITLE[0] + '【会员管理】' + '【' + users_manage_list[1] + '】' + '******')
-            with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                if len(f.readlines(1)) == 1:
-                    with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                        j = 1
-                        for i in f:
-                            u, v = i.split(':')
-                            print(u)
-                            j += 1
-                            if j == PER_PAGE_AMOUNT +1:
-                                j = 1
-                                input('按enter继续')
-                else:
-                    print('没有用户')
-                input('按enter结束')
-                return
-        """
-        1.3修改密码
-        """
-        def modify_user_pwd():
-            while True:
-                print('******' + TITLE[0] + '【会员管理】' + '【' + users_manage_list[2] + '】' + '******')
-                user_name = input('输入用户名(N退出): ')
-                if user_name.upper() == 'N':
-                    break
-                if user_name.strip() == '':
-                    print('不合法用户名')
-                    continue
-
-                with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                    a = '用户不存在'
-                    for i in f:
-                        u, v = i.strip().split(':')
-                        if user_name != u:
-                            pass
-                        else:
-                            a = True
-                    if a == '用户不存在':
-                        print(a)
-                        continue
-                    else:
-                        with open('user_list.txt', mode='r', encoding='utf-8') as f, \
-                                open('user_list1.txt', mode='w', encoding='utf-8') as f1:
-                            if len(f.readlines(1)) == 1:
-                                with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                                    for i in f:
-                                        u, v = i.strip().split(':')
-                                        if user_name == u:
-                                            while True:
-                                                user_pwd = input('请输入密码: ')
-                                                user_pwd2 = input('请确认密码: ')
-                                                if user_pwd != user_pwd2:
-                                                    print('两次密码不一致')
-                                                    continue
-                                                else:
-                                                    break
-                                            user_pwd = get_data_md5(user_pwd)
-
-                                            f1.write(user_name + ':' + user_pwd + '\n')
-                                        else:
-                                            f1.write(i)
-                                            f1.flush()
-                        # 删除旧文件，并重新命名新文件
-                        import os
-                        os.remove('user_list.txt')
-                        os.rename('user_list1.txt', 'user_list.txt')
-            return
-        """
-        1.4删除用户
-        """
-        def user_del():
-            while True:
-                print('******' + TITLE[0] + '【会员管理】' + '【' + users_manage_list[3] + '】' + '******')
-                user_name = input('输入用户名(N退出): ')
-                if user_name.upper() == 'N':
-                    break
-                if user_name.strip() == '':
-                    print('不合法用户名')
-                    continue
-
-                with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                    a = '不存在'
-                    for i in f:
-                        u, v = i.strip().split(':')
-                        if user_name != u:
-                            pass
-                        else:
-                            a = True
-                    if a == '不存在':
-                        print(a)
-                        continue
-
-                with open('user_list.txt', mode='r', encoding='utf-8') as f, \
-                        open('user_list1.txt', mode='w', encoding='utf-8') as f1:
-                    if len(f.readlines(1)) == 1:
-                        with open('user_list.txt', mode='r', encoding='utf-8') as f:
-                            for i in f:
-                                u, v = i.strip().split(':')
-                                if user_name == u:
-                                    print('用户已删除')
-                                else:
-                                    f1.write(i)
-                                    f1.flush()
-                # 删除旧文件，并重新命名新文件
-                import os
-                os.remove('user_list.txt')
-                os.rename('user_list1.txt', 'user_list.txt')
-                return
-        ''
-        while True:
-            print('******' + TITLE[0] + '】' + TITLE[5] + '】******')
-            users_manage_funcs = [user_add, users_list, modify_user_pwd, user_del]
-            display_pages(users_manage_list)
-            num = input('请选择输入N返回: ')
-            v = just_user_input(num, users_manage_funcs)
-            if v == "N":
-                return
-            elif v == FAULT:
-                print(v)
-            else:
-                users_manage_funcs[v]()
+        print('正在开发中，不可用')
 
     """
     5. 首页展示
@@ -780,7 +574,7 @@ def mall_manage():
         print('******', TITLE[0], '******')
         li = ['商品管理', '会员管理']
         display_pages(li)
-        num = input('please input your choice: ')
+        num = input('请选择（输入N返回）: ')
         funcs = [goods_manage, users_manage]
         v = just_user_input(num, li)
         if v == 'N':
@@ -789,9 +583,7 @@ def mall_manage():
             print(v)
         else:
             funcs[v]()
-
-# mall_manage() if user_log_on() == '登陆成功' else exit()
-mall_manage()
+mall_manage_sys()
 
 
 
