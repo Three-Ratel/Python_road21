8fdc0e0a58892a28691e5e120c572a9a6fa850e5
henry <958976577@qq.com>
2019-04-23 20:25:05 +0800

    day019

--- day019 面向对象/1.查看实例所在类.py
+++ day019 面向对象/1.查看实例所在类.py
@@ -2,13 +2,6 @@
 # -*- coding:utf-8 -*-
 
 
-class Test:
-    def prt(self):
-        print(self)
-        print(self.__class__)
 
-
-t = Test()
-t.prt()
 
 
--- day019 面向对象/test.py
+++ day019 面向对象/test.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-dict
-
-
-list
-
-set
 
