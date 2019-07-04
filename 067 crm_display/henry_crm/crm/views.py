96805c3e4eccebfd4fdaf4a8baddd6abfc7cb6bf
henry <958976577@qq.com>
2019-07-05 00:58:52 +0800

    django-paginator

--- 067 crm_display/henry_crm/crm/views.py
+++ 067 crm_display/henry_crm/crm/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect, reverse
 
 from crm import models
 from crm.forms import RegForm
-from utils.pagenation import Pagenation
 
 
 def reg(request):
@@ -28,7 +27,7 @@ def login(request):
         md.hexdigest()
         obj = models.UserProfile.objects.filter(username=username, password=md.hexdigest(), is_active=True).first()
         if obj:
-            return redirect('/index/')
+            return redirect('customer_list')
         return render(request, 'login.html', {'error': '用户名或密码错误'})
     return render(request, 'login.html')
 
@@ -37,7 +36,27 @@ def index(request):
     return render(request, 'index.html')
 
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+items = models.Customer.objects.all()
+
+
 def customer_list(request):
-    all_item = models.Customer.objects.all()
-    obj = Pagenation(request, len(all_item))
-    return render(request, 'customer_list.html', {'all_item': all_item[obj.start:obj.end], 'all_page': obj.show,})
+    # all_item = models.Customer.objects.all()
+    # obj = Pagenation(request, len(all_item))
+    # return render(request, 'customer_list.html', {'all_item': all_item[obj.start:obj.end], 'all_page': obj.show})
+
+    """使用django的分页器"""
+
+    paginator = Paginator(items, 10)
+    page = request.GET.get('page')
+    try:
+        all_item = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        all_item = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        all_item = paginator.page(paginator.num_pages)
+    return render(request, 'customer_list.html', {'all_item': all_item})
+    # return render(request, 'customer_list.html', {'all_item': p.page(page).object_list, 'all_page': })
