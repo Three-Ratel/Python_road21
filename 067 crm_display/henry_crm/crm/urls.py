ba282ad21505f06bcca0a7c013232729ac17fcbf
henry <958976577@qq.com>
2019-07-04 13:11:50 +0800

    display_customer

---
+++ 067 crm_display/henry_crm/crm/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from crm import views
+
+urlpatterns = [
+    url(r'^login/', views.login, name='login'),
+    url(r'^reg/', views.reg, name='reg'),
+    url(r'^customer_list/', views.customer_list, name='customer_list'),
+]
