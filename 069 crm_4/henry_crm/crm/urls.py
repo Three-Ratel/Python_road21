7923de5f7ef51856608fc566e437dfaa782e337f
henry <958976577@qq.com>
2019-07-08 17:45:35 +0800

    4requirements

---
+++ 069 crm_4/henry_crm/crm/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from crm import views
+
+urlpatterns = [
+    url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^reg/', views.reg, name='reg'),
+    url(r'^customer/', views.customer_list, name='customer'),
+    url(r'^show_customer/', views.customer_list, name='show_customer'),
+    url(r'^add_customer/', views.modify_customer, name='add_customer'),
+    url(r'^search/', views.customer_list, name='search'),
+    url(r'^edit_customer/(\d+)', views.modify_customer, name='edit_customer'),
+    url(r'^del/', views.del_item, name='del'),
+    url(r'^transfer/', views.transfer, name='transfer'),
+
+]
