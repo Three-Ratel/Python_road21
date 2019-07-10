73882c63e717f8fbad2dfdc5a9d32fb8c9424e77
henry <958976577@qq.com>
2019-07-10 10:57:46 +0800

    rename

--- projects/8.henry_crm/henry_crm/settings.py
+++ projects/8.henry_crm/henry_crm/settings.py
@@ -107,14 +107,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = False
 
 USE_TZ = True
+
 DATE_FORMAT = 'Y/m/d'
+DATETIME_FORMAT = 'Y/m/d H:i:s'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
