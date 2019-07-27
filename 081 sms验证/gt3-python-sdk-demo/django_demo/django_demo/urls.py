4502dc785e91ea06cc0f15c7c8b441e6213714c7
henry <958976577@qq.com>
2019-07-27 17:17:08 +0800

    sms

--- 081 sms验证/gt3-python-sdk-demo/django_demo/django_demo/urls.py
+++ 081 sms验证/gt3-python-sdk-demo/django_demo/django_demo/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-	url(r'^pc-geetest/register', views.pcgetcaptcha, name='pcgetcaptcha'),
-	# url(r'^mobile-geetest/register', views.mobilegetcaptcha, name='mobilegetcaptcha'),
+	url(r'^pc-geetest/register', views.pcgetcaptcha, name='pcgetcaptcha'), # 相当于初始化
 	url(r'^pc-geetest/validate$', views.pcvalidate, name='pcvalidate'),
-	url(r'^pc-geetest/ajax_validate', views.pcajax_validate, name='pcajax_validate'),
-	# url(r'^mobile-geetest/ajax_validate', views.mobileajax_validate, name='mobileajax_validate'),
+	url(r'^pc-geetest/ajax_validate', views.pcajax_validate, name='pcajax_validate'), # 二次验证
 	url(r'', views.home, name='home'),
 ]
